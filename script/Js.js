b9a743500f2b744485bae59fd91b0707800ef480
ThamiduChamod <thamiduchamod748@gmail.com>
2025-05-22 00:37:27 +0530

    update js

--- script/Js.js
+++ script/Js.js
@@ -2,7 +2,7 @@ function imageOverlay() {
     var imgHeight = $('#mainImg').height();
     $('#overlayDiv').height(imgHeight);
     $('#home').height(imgHeight);
-    $('#aboutMe').css('top', imgHeight + 'px'); // ← මෙහෙම වෙනස් කරන්න
+    // $('#aboutMe').css('top', imgHeight + 'px'); // ← මෙහෙම වෙනස් කරන්න
     var imgWidth = $('#mainImg').width();
     $('#overlayDiv').width(imgWidth - 2);
     console.log(imgWidth);
