545f1b05a2b61b93ee618f639ce688ae2d7a6334
ThamiduChamod <thamiduchamod748@gmail.com>
2025-05-21 12:33:09 +0530

    add new css for about me

--- script/Js.js
+++ script/Js.js
@@ -1,14 +1,12 @@
+function imageOverlay() {
+    var imgHeight = $('#mainImg').height();
+    $('#overlayDiv').height(imgHeight);
+    $('#home').height(imgHeight);
+    $('#aboutMe').css('top', imgHeight + 'px'); // ← මෙහෙම වෙනස් කරන්න
+    var imgWidth = $('#mainImg').width();
+    $('#overlayDiv').width(imgWidth - 2);
+    console.log(imgWidth);
+}
 
-    function imageOverlay() {
-
-        var imgHeight = $('#mainImg').height();
-        $('#overlayDiv').height(imgHeight);
-        console.log(imgHeight);
-        var imgWidth = $('#mainImg').width();
-        $('#overlayDiv').width(imgWidth-2);
-        console.log(imgWidth);
-
-    }
-
-    window.addEventListener("load", imageOverlay)
-    window.addEventListener("resize", imageOverlay)
+window.addEventListener("load", imageOverlay);
+window.addEventListener("resize", imageOverlay);
