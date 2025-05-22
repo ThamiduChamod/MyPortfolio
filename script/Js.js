ba4dfb3df4c3c8dc62f92a29d0d67c59e90bc90f
ThamiduChamod <thamiduchamod748@gmail.com>
2025-05-22 13:08:08 +0530

    remove old css and js

--- script/Js.js
+++ script/Js.js
@@ -14,37 +14,37 @@ window.addEventListener('resize', updateWindowSize);
 
 
 
-
-function imageOverlay() {
-
-    var imgWidth = $('#mainImg').width();
-    $('#overlayDiv').width(imgWidth - 2);
-    console.log(imgWidth);
-    // if (imgWidth > )
-    var imgHeight = $('#mainImg').height();
-    $('#overlayDiv').height(imgHeight);
-    $('#home').height(imgHeight);
-    $('#aboutMe').css('top', imgHeight+10 + 'px');
-    var aboutHeight = $('#aboutMe').height();
-    $('#myWorks').css('top', imgHeight+aboutHeight+10 + 'px');
-    var worksHeight = $('#myWorks').height();
-    $('#skill').css('top', imgHeight+aboutHeight+worksHeight+10 + 'px');
-    var skillHeight = $('#skill').height();
-    $('#contactme').css('top', imgHeight+aboutHeight+worksHeight+skillHeight+10 + 'px');
-
-
-
-    // if (screen_width <)
-
-
-
-}
+//
+// function imageOverlay() {
+//
+//     var imgWidth = $('#mainImg').width();
+//     $('#overlayDiv').width(imgWidth - 2);
+//     console.log(imgWidth);
+//     // if (imgWidth > )
+//     var imgHeight = $('#mainImg').height();
+//     $('#overlayDiv').height(imgHeight);
+//     $('#home').height(imgHeight);
+//     $('#aboutMe').css('top', imgHeight+10 + 'px');
+//     var aboutHeight = $('#aboutMe').height();
+//     $('#myWorks').css('top', imgHeight+aboutHeight+10 + 'px');
+//     var worksHeight = $('#myWorks').height();
+//     $('#skill').css('top', imgHeight+aboutHeight+worksHeight+10 + 'px');
+//     var skillHeight = $('#skill').height();
+//     $('#contactme').css('top', imgHeight+aboutHeight+worksHeight+skillHeight+10 + 'px');
+//
+//
+//
+//     // if (screen_width <)
+//
+//
+//
+// }
 
 window.addEventListener("resize", imageOverlay);
 
 
 window.addEventListener("load", function () {
-    imageOverlay();
+    // imageOverlay();
     $('#self-btn-div').css('display','block');
     $('#edu-btn-div').css('display','none');
 
